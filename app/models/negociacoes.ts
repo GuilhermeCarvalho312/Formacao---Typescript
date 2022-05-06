021159bd16ea2d29823e3c08fb4c06e9f22e30ab
Guilherme Augusto de Carvalho Silva <guilhermeacarvalhosilva@gmail.com>
2022-05-05 21:22:36 -0300

    Aula - 5 - Final do curso_1

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,14 +1,16 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-  private negociacoes: Array<Negociacao> = [];
+  private negociacoes: Negociacao[] = []; // [] é a mesma coisa que Array<>
 
   addNegotiation(negociacao: Negociacao) {
     //Somente é preciso especificar o retorno em métodos que precisam de retorno
     this.negociacoes.push(negociacao);
   }
 
-  lista(): ReadonlyArray<Negociacao> { // Readonly(somente leitura) não permite modificar o array de retorno da função
+  lista(): readonly Negociacao[] {
+    // ReadonlyArray<nomeArray> mesma coisa
+    // Readonly(somente leitura) não permite modificar o array de retorno da função. readonly nomeArray[] é a mesma coisa que a frase anterior
     return this.negociacoes;
   }
 }
